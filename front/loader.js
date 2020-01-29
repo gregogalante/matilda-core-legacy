ec8e69fcb643822f811be1769d70a8073bd983f7
Gregorio Galante <me@gregoriogalante.com>
2020-01-29 18:21:09 +0100

    Update generale

--- front/loader.js
+++ front/loader.js
@@ -1,8 +1,2 @@
-import './scripts/main.js'
-
-import '@fortawesome/fontawesome-free/css/all.min.css'
-import 'tippy.js/dist/tippy.css'
-import './style/main.scss'
-
-import './images/locales/_en.png'
-import './images/locales/_it.png'
+import './loader_assets'
+import './loader_code'
---
+++ front/loader_assets.js
@@ -0,0 +1,4 @@
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import 'tippy.js/dist/tippy.css'
+import './images/locales/_en.png'
+import './images/locales/_it.png'
