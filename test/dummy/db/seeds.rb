93b0b4e930b93b29303857748ec51dad5e1e7f49
Gregorio Galante <me@gregoriogalante.com>
2020-03-05 22:24:01 +0100

    Update generale

--- test/dummy/db/seeds.rb
+++ test/dummy/db/seeds.rb
@@ -1,19 +1,16 @@
 # frozen_string_literal: true
 
-# command_create_group = MatildaCore::Groups::CreateGroupCommand.new(
-#   name: 'Matilda development group'
-# )
-# throw command_create_group.error_messages.to_sentence unless command_create_group.completed?
-
-# command_invite_member = MatildaCore::AppMemberships::InviteMemberCommand.new(
-#   group_uuid: command_create_group.group_uuid,
-#   name: 'Sviluppatore',
-#   surname: '1d3o',
-#   email: 'me@gregoriogalante.com'
-# )
-# throw command_invite_member.error_messages.to_sentence unless command_invite_member.completed?
-
 MatildaCore::Group.create(
   uuid: 'default',
-  name: 'Gruppo di sviluppo'
+  name: 'Dev group'
+)
+MatildaCore::User.create(
+  uuid: 'admin',
+  username: 'admin',
+  password: BCrypt::Password.create('Password1!')
+)
+MatildaCore::Membership.create(
+  user_uuid: 'admin',
+  group_uuid: 'default',
+  permissions: ['matilda_core.memberships']
 )
