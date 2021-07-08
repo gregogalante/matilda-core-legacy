d53c87b205074b233b257b24ff481db2a7bff445
Gregorio Galante <me@gregoriogalante.com>
2021-07-08 18:09:27 +0200

    General update

--- front/matilda_core/components/MatildaTable.jsx
+++ front/matilda_core/components/MatildaTable.jsx
@@ -58,6 +58,7 @@ export function useMatildaTable (configProps = {}) {
       columns: [],
       data: [],
       route: null,
+      routeExtraParams: {},
       routeDataParser: () => [],
       routePaginationParser: null,
       pagination: false,
@@ -89,6 +90,7 @@ export function useMatildaTable (configProps = {}) {
       sort_field: sort.field || '',
       sort_order: {ascend:'ASC',descend:'DESC'}[sort.order] || 'ASC'
     }
+    params = Object.assign(params, config.routeExtraParams)
 
     // invio richiesta
     request.send(config.route, params).then((response) => {
