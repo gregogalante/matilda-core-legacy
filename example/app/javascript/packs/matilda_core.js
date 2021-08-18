0c2bece346f3a61718696459837f91b1b8704192
Gregorio Galante <me@gregoriogalante.com>
2021-08-18 17:18:03 +0200

    Update matilda_core.js

--- example/app/javascript/packs/matilda_core.js
+++ example/app/javascript/packs/matilda_core.js
@@ -1,4 +1,4 @@
 // Support component names relative to this directory:
-var matildaCoreRequireContext = require.context('../../../vendor/matilda_core', true);
+var matildaCoreRequireContext = require.context('../matilda_core', true);
 var ReactRailsUJS = require('react_ujs');
 ReactRailsUJS.useContext(matildaCoreRequireContext);
